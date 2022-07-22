c857721cf6a1d18fc21b393f23434f08d580a373
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-07-21 22:40:46 -0500

    dart variables

--- index.dart
+++ index.dart
@@ -1,3 +1,15 @@
 void main() {
   print('Hello, World!');
 }
+
+// How to declare variables
+
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
