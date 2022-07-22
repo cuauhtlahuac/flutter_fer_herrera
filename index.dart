eef725f87001c665fad81e1253cceed07e3f3bbd
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-07-21 22:41:00 -0500

    dart control flow

--- index.dart
+++ index.dart
@@ -13,3 +13,22 @@ var image = {
   'url': '//path/to/saturn.jpg'
 };
 
+// Control flow statements
+
+if (year >= 2001) {
+  print('21st century');
+} else if (year >= 1901) {
+  print('20th century');
+}
+
+for (final object in flybyObjects) {
+  print(object);
+}
+
+for (int month = 1; month <= 12; month++) {
+  print(month);
+}
+
+while (year < 2016) {
+  year += 1;
+}
