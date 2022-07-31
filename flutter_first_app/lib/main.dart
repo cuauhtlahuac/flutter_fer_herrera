d03a78f4aa53c63f2866d30bb317390a2c17bf44
Cuauhtlahuac macbookpro <cuitlagua@gmail.com>
2022-07-31 12:03:46 -0500

    style: create and import widget

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/screens/home_screen.dart';
 
 main() {
   // runApp es un widget que recibe de parámetro otro widget
   // se importa de material
-  runApp(MyFirstWidget());
+  runApp(const MyFirstWidget());
 }
 
 // Aqui definimos nuestro primer widget
 // Para que sea un widget se tiene que extender a Stateless o Stateful widget
 class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // ctrl + espaciadora para ver las propiedades
-    return MaterialApp(
-      home: Text('Hola'),
-    );
+    return const MaterialApp(home: HomeScreen());
   }
 }
