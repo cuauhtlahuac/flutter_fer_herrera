24b4f9aced34f6154e52605a34b51a471b90efd9
Cuauhtlahuac macbookpro <cuitlagua@gmail.com>
2022-07-31 13:21:15 -0500

    style: center text with one issue

--- flutter_first_app/lib/screens/home_screen.dart
+++ flutter_first_app/lib/screens/home_screen.dart
@@ -2,11 +2,29 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
+  final int _count = 0;
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Screen Aña'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aña App counter $_count'),
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment es relativo al widget que tiene mayor tamaño
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Text('first Aña'),
+          Text('second Aña'),
+          Text('third Aña'),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => null,
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
